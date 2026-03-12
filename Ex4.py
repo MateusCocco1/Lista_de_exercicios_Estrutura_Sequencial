912742ff4baee486c460b4b65bb81f67d1b5597a
MateusCocco1 <mateus.cocco@aluno.impacta.edu.br>
2026-03-12 19:38:58 -0300

    Ex4.py

---
+++ Ex4.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+n3 = float(input("Digite a terceira nota: "))
+n4 = float(input("Digite a quarta nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print("A média das notas é:", media)
