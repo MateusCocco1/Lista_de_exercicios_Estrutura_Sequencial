7bc147f4bf8d05c2d215bb315f0bcecc527404b2
MateusCocco1 <mateus.cocco@aluno.impacta.edu.br>
2026-03-12 19:41:09 -0300

    Ex8.py

---
+++ Ex8.py
@@ -0,0 +1,6 @@
+valor_hora = float(input("Quanto você ganha por hora? "))
+horas_mes = float(input("Quantas horas você trabalhou no mês? "))
+
+salario = valor_hora * horas_mes
+
+print("O salário total do mês é:", salario)
